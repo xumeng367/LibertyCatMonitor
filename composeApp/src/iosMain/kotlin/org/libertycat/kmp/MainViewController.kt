902be93d2fecc86e30199233599c9852ad137db5
xumeng <xumeng@u-care.net.cn>
2024-10-28 12:58:05 +0800

    init

---
+++ composeApp/src/iosMain/kotlin/org/libertycat/kmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.libertycat.kmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ shared/src/commonMain/kotlin/org/libertycat/kmp/Constants.kt
@@ -0,0 +1,3 @@
+package org.libertycat.kmp
+
+const val SERVER_PORT = 8080
